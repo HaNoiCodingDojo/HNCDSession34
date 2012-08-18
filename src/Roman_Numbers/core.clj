14ffc8616e54c87485799accbf598e0e3a62dd50
Dương H. Nguyễn <cmpitg@gmail.com>
2012-08-18 14:49:25 +0700

    Initial commit

---
+++ src/Roman_Numbers/core.clj
@@ -0,0 +1,6 @@
+(ns Roman-Numbers.core)
+
+(defn -main
+  "I don't do a whole lot."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/Roman_Numbers/core_test.clj
@@ -0,0 +1,7 @@
+(ns Roman-Numbers.core-test
+  (:use clojure.test
+        Roman-Numbers.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
