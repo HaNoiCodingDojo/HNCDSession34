11386d82c9b6904fc4161de9c62950e30688fa69
Dương H. Nguyễn <cmpitg@gmail.com>
2012-08-18 15:01:03 +0700

    myguidingstar

--- test/Roman_Numbers/core_test.clj
+++ test/Roman_Numbers/core_test.clj
@@ -2,6 +2,12 @@
   (:use clojure.test
         Roman-Numbers.core))
 
+(defn arabic->roman
+  "Convert arabic numbers to roman ones"
+  [number]
+  (if (= 1 number)
+    "I"))
+
 (deftest a-test
   (testing "1 becomes I"
     (is (= "I" (arabic->roman 1)))))
